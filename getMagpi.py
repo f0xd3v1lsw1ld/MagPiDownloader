695b8dea2b3a8b8dc1e201b7a6656e079724b7f6
f0xd3v1lsw1ld <f0xd3v1lsw1ld@gmail.com>
2016-08-27 11:30:36 +0200

    add progress bar

--- getMagpi.py
+++ getMagpi.py
@@ -4,24 +4,35 @@ __author__ = "f0xd3v1lsw1ld@gmail.com"
 # first install beautifulsoup
 # use: sudo pip install beautifulsoup4 
 
-import requests, bs4, os
+import requests, bs4, os, sys, time
 
 url = 'https://www.raspberrypi.org/magpi-issues/'
 
 
 def download(url, file):
-    res = requests.get(url + file)
-    try:
-        res.raise_for_status()
-    except Exception as exc:
-        print('There was a problem: %s' % (exc))
-        return False
 
-    magpiFile = open(file, 'wb')
-    for chunk in res.iter_content(100000):
-        magpiFile.write(chunk)
-    magpiFile.close()
+    with open(file, 'wb') as magpiFile:
+        start = time.clock()
+        res = requests.get(url + file, stream=True)
+        try:
+            res.raise_for_status()
+        except Exception as exc:
+            print('There was a problem: %s' % (exc))
+            return False
 
+        total_length = res.headers.get('content-length')
+        download_cnt = 0
+        if total_length is None: # no content length header
+            magpiFile.write(res.content)
+        else:
+            for chunk in res.iter_content(1024):
+                download_cnt += len(chunk)
+                done = int(50 * download_cnt / int(total_length))
+                sys.stdout.write("\r[%s%s] %s bps" % ('=' * done, ' ' * (50-done), download_cnt//(time.clock() - start)))
+                sys.stdout.flush()
+
+    sys.stdout.write("\n")
+    return True
 
 def main():
     res = requests.get(url)
